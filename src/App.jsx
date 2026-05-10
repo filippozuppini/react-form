ae7f4b1af9d8f93368f8ab3ddbbccc86419b3c60
filippozuppini <zuppini.filippo@gmail.com>
2026-05-10 19:17:07 +0200

    add list and map for print alla the articles in the array

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,50 @@
-import { useState } from 'react'
-import './App.css'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const articles = [
+    {
+      id: 1,
+      title: 'Article1'
+    },
+    {
+      id: 2,
+      title: 'Article2'
+    },
+    {
+      id: 3,
+      title: 'Article3'
+    },
+    {
+      id: 4,
+      title: 'Article4'
+    },
+  ]
 
   return (
     <>
+
+
+      <h1 className="pageTitle">Articles List</h1>
+
+
+      <div className="content">
+      {
+        articles.map((article) => (
+          <div className="article" key={article.id}>
+            <h1>{article.title}</h1>
+          </div>
+
+        ))
+      }
+      </div>
+
+
+
+
+
+
+
 
     </>
   )
