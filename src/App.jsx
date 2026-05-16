fd3843fcc273d1aeb0e75fee9b0306578d8c0ba2
filippozuppini <zuppini.filippo@gmail.com>
2026-05-16 19:54:01 +0200

    add form function

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-
+import { useState } from "react";
 
 function App() {
 
-  const articles = [
+  const initialArticles = [
     {
       id: 1,
       title: 'Article1'
@@ -18,8 +18,28 @@ function App() {
     {
       id: 4,
       title: 'Article4'
-    },
+    }
   ]
+
+
+  function handleSubmit (e){
+    e.preventDefault()
+    console.log('Form inviato');
+
+    const newArticle ={
+      id: articles.length + 1,
+      title: newArticles,
+    };
+
+    setArticles([...articles, newArticle])
+    
+    setNewArticles('')
+  }
+
+
+  const [articles, setArticles] = useState(initialArticles)
+  const [newArticles, setNewArticles] = useState('')
+
 
   return (
     <>
@@ -29,15 +49,31 @@ function App() {
 
 
       <div className="content">
-      {
-        articles.map((article) => (
-          <div className="article" key={article.id}>
-            <h1>{article.title}</h1>
-          </div>
 
-        ))
-      }
+
+        <form onSubmit={handleSubmit}>
+          <input name="query"  placeholder="Inserisci titolo" value={newArticles} onChange={e => setNewArticles(e.target.value)} />
+
+          {/* <input name="query" placeholder="Inserisci titolo" onChange={setNewArticles} /> */}
+          <button type="submit">Inserisci</button>
+        </form>
+
+
+        <div className="articles">
+        {
+          articles.map((article,i) => (
+            <div className="article" key={article.id}>
+              <h1>{article.title}</h1>
+            </div>
+
+          ))
+        }
+        </div>
+
       </div>
+
+
+
 
 
 
